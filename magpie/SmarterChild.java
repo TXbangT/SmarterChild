d99dc2eebee80e20cb008bbc598b7da455b74995
raothman <ramsey.othman@gmail.com>
2016-03-22 14:21:13 -0400

    Update SmarterChild.java

--- magpie/SmarterChild.java
+++ magpie/SmarterChild.java
@@ -14,7 +14,7 @@ public class SmarterChild extends Main{
 	private void getName(){
 		System.out.println("What is your name?");
 		info.add(new Information("Name", s.next()));
-		System.out.println("Hello " + returnName()+ I am a smarter child of Magpie.");
+		System.out.println("Hello " + returnName()+ "/nI am a smarter child of Magpie.");
 	}
 	
 	private ArrayList<String> getGames(){
