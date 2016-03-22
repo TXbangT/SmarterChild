a35e90418f92c7e3a2659942319789ac291bba2a
TXbangT <TXbangT@gmail.com>
2016-03-22 14:08:38 -0400

    Update Game.java

--- magpieAssist/Game.java
+++ magpieAssist/Game.java
@@ -2,15 +2,30 @@ package magpieAssist;
 
 public interface Game {
 	
+	/**
+	 * Returns the name of the game 
+	 */
 	public String getName();
 	
+	/**
+	 *  Ends the game and determines a winner 
+	 */
 	public String endGame();
 	
+	/**
+	 * Starts the game
+	 */
 	public void play();
 	
+	/**
+	 * Makes a move for the computer
+	 */
 	public String computerMove();
 	
-	public String move();
+	/**
+	 * Makes a move given a string input
+	 */
+	public String move(String move);
 	
 
 }
