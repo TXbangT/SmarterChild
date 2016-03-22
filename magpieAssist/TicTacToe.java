9b9dfc36d4bc0cfa0fcbf01925c181159a7d088f
TXbangT <TXbangT@gmail.com>
2016-03-22 14:22:36 -0400

    Update TicTacToe.java

--- magpieAssist/TicTacToe.java
+++ magpieAssist/TicTacToe.java
@@ -1,10 +1,33 @@
 package magpieAssist;
 
-public class TicTacToe{
+import java.util.*;
+public class TicTacToe implements Game{
   private int[] info = new int [3][3];
-private String[][] board = {{" ", "|", " ", "|", " "},
-					{"-", "-", "-" , "-", "-"},
-					{" ", "|", " ", "|", " "},
-					{"-", "-", "-" , "-", "-"},
-					{" ", "|", " ", "|", " "}};
+  private String name = "TicTacToe";
+  private Scanner s = new Scanner(System.in);
+  private int x;
+  private int y;
+  public TicTacToe(){
+  }
+  /**
+   * Starts the game
+   */
+  public void play(){
+  	System.out.println("You are X Computer is O");
+  	while(checkWin()==0()){
+  		System.out.println("Input the location of your play");
+  		System.out.print("X: ");
+  		s = s.nextInt();
+  		
+  	}
+  }
+  
+  /**
+   * @Return 0 if no win; 1 if X win; 2 if O win; -1 if tie;
+   */
+  public int checkWin(){
+  	if(info[0][0]==info[1][1]&&info[2][2]=info[0][0]){
+  		
+  	}
+  }
 }
