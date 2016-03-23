48e80f54802058b8044d8739b697c2731db77d85
GJ2000 <gjentis@yahoo.com>
2016-03-22 21:57:58 -0400

    Update PigLatin.java

--- magpieAssist/PigLatin.java
+++ magpieAssist/PigLatin.java
@@ -3,5 +3,42 @@ package magpieAssist;
 public class PigLatin
 {
 private static String final AY = "ay";
-private String firstLetter = "";
+private String translated = "";
+private String start;
+private String word = ""
+
+public PigLatin(String s)
+{
+  start = s;
+}
+
+public String convertWord(String w){
+  return w.substring(1) + w.substring(0,1) + AY;
+}
+
+
+//I need something to deal with punctuation
+public void translate()
+{
+  while(start.length > 0)
+  {
+    if(start.indexOf(" ") > 0 && start.indexOf(" ") < start.length())
+    {
+    word = start.substring(0, start.indexOf(" "));
+    start -= word;
+    translated = convertWord(word) + " ";
+    }
+    else
+    {
+    word = start;
+    start -= word;
+    translated = convertWord(word);
+    }
+  }
+}
+public String getTranslated()
+{
+  return translated;
+}
+
 }
