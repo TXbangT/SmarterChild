aea012d6d700ebe624c0498dc7ecac3c7d359904
Sean123123123 <smmasur@gmail.com>
2016-03-23 13:47:17 -0400

    Update SmarterChildRunner.java

--- magpie/SmarterChildRunner.java
+++ magpie/SmarterChildRunner.java
@@ -9,5 +9,14 @@ public class SmarterChildRunner {
     SmarterChild child= new SmarterChild();
     child.
     Scanner s= new Scanner(in);
+    String statement= s.nextLine();
+    
+    while (!statement.toUpperCase().equals("BYE")&&(!statement.toUpperCase().equals("GOODBYE")))
+		{
+			System.out.println (maggie.getResponse(statement));
+			statement = in.nextLine();
+		}
+
+                in.close();
   }
 }
