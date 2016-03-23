004e4c90b49f54b1c7dfcbed8a1d18f836796dfd
TXbangT <TXbangT@gmail.com>
2016-03-22 21:05:43 -0400

    Update TicTacToe.java

--- magpieAssist/TicTacToe.java
+++ magpieAssist/TicTacToe.java
@@ -14,7 +14,7 @@ public class TicTacToe implements Game{
    */
   public void play(){
   	System.out.println("You are X Computer is O");
-  	while(checkWin()==0()){
+  	while(checkWin()==-1()){
   		System.out.println("Input the location of your play");
   		System.out.print("X: ");
   		s = s.nextInt();
@@ -23,11 +23,37 @@ public class TicTacToe implements Game{
   }
   
   /**
-   * @Return 0 if no win; 1 if X win; 2 if O win; -1 if tie;
+   * @Return -1 if no win; 0 if X win; 1 if O win; 2 if tie;
    */
   public int checkWin(){
+    int winTile -1;
   	if(info[0][0]==info[1][1]&&info[2][2]=info[0][0]){
-  		
+  		winTile = info[0][0];
+  	}else if(info[0][2]==info[1][1]&&info[2][0]=info[0][2]){
+  	  winTile = info[1][1];
+  	}else{
+  	  for(int i = 0; i<3; i++){
+  	    if(info[0][i]==info[1][i]&&info[2][i]==info[0][i]){
+  	      winTile = info[0][i];
+  	      break;
+  	    }
+  	  }
+  	    for(int i = 0; i<3; i++){
+  	      if(info[i][0]==info[i][1]&&info[i][2]==info[i][0]){
+  	        winTile = info[i][0];
+  	        break;
+  	      }
+  	    }
+  	  }
+  	  for(int i = 0; i<3; i++){
+  	    for(int j = 0; j<3; j++){
+  	      if(info[i][j]==0){
+  	        winTile = 2;
+  	        i=4;
+  	        break;
+  	      }
+  	    }
+  	  }
+  	  return winTile;
   	}
   }
-}
